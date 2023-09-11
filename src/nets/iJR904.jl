d27e7b9318dd1e3153a225cd9eb1e3ba87f25d1a
josePereiro <jose.pereiro.personal@gmail.com>
2023-09-11 04:22:40 -0400

    raw iJO904 and iJ1366 from json

--- src/nets/iJR904.jl
+++ src/nets/iJR904.jl
@@ -1,18 +1,18 @@
 function _iJR904_builder()
 
     # load
-    net = _load_raw_model("iJR904.xml")
+    net = _load_raw_model("iJR904.json")
     net = _common_format(net)
     
-    extras!(net, "BIOM", "R_BIOMASS_Ecoli")
-    extras!(net, "EX_GLC", "R_EX_glc__D_e")
-    extras!(net, "EX_NH4", "R_EX_nh4_e")
-    extras!(net, "EX_GLU", "R_EX_glu__L_e")
-    extras!(net, "EX_O2", "R_EX_o2_e")
-    extras!(net, "EX_CO2", "R_EX_co2_e")
-    extras!(net, "ATPM", "R_ATPM")
+    extras!(net, "BIOM", "BIOMASS_Ecoli")
+    extras!(net, "EX_GLC", "EX_glc__D_e")
+    extras!(net, "EX_NH4", "EX_nh4_e")
+    extras!(net, "EX_GLU", "EX_glu__L_e")
+    extras!(net, "EX_O2", "EX_o2_e")
+    extras!(net, "EX_CO2", "EX_co2_e")
+    extras!(net, "ATPM", "ATPM")
     
-    linear_weights!(net, "R_BIOMASS_Ecoli", 1.0)
+    linear_weights!(net, "BIOMASS_Ecoli", 1.0)
 
     return net
 end
