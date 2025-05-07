d2f375f225b7d9896c31d71454cdfdd8013707dd
josePereiro <jose.pereiro.personal@gmail.com>
2025-05-06 23:15:39 -0400

    WIP iML1515 + PAMModel-alterProteomeRegulationPatterns2021

--- src/nets/PAMModel-alterProteomeRegulationPatterns2021.jl
+++ src/nets/PAMModel-alterProteomeRegulationPatterns2021.jl
@@ -2,7 +2,18 @@ using JSON
 using CSV
 using DataFrames
 
-function __PAMModel_alterProteomeRegulationPatterns2021_builder(build_args...)
+function __PAMModel_alterProteomeRegulationPatterns2021_builder(
+        options::Dict = Dict()
+    )
+    
+    ## - --- -- . .- -.-. -.- .-.-. 
+    # extract options
+    f_UE0 = get(options, "f_UE0", 0.17)                 # [g / gCDW]
+    u_ider = get(options, "u_ider", "EX_glc__D_e_b")    # nutrinet ider
+    u_max = get(options, "u_max", 8.9)                  # [mmol/ h gCDW] max 
+    f_T0 = get(options, "f_T0", 50.0)                   # [mg / gCDW]
+    w_T = get(options, "w_T", 36.8)                     # [mg h / gCDW]
+    f_Pc = get(options, "f_Pc", 0.26)                   # [g / gCDW]
     
     ## - --- -- . .- -.-. -.- .-.-. 
     # MARK: Load iML1515
@@ -12,26 +23,7 @@ function __PAMModel_alterProteomeRegulationPatterns2021_builder(build_args...)
     S0, b0, lb0, ub0, mets0, rxns0 = net0.S, net0.b, net0.lb, net0.ub, net0.mets, net0.rxns
 
     ## - --- -- . .- -.-. -.- .-.-. 
-    # Split reactions
-    # S_f v_f + S_b v_b = 0
-    # S_f = S0
-    # S_b = -S0
-    # v = v_f - v_b
-
-    rxns1 = [
-        [string(rxn, "_f") for rxn in rxns0]; 
-        [string(rxn, "_b") for rxn in rxns0]; 
-    ]
-    mets1 = mets0
-    S1 = [S0 -S0]
-    M, N = size(S1)
-    b1 = b0
-    c1 = zeros(N)
-    lb1 = zeros(N)
-    ub1 = [ub0; abs.(lb0)]
-
-    ## - --- -- . .- -.-. -.- .-.-. 
-    # MARK: PaperSON
+    # MARK: Load PaperSON
     # Load data from alterProteomeRegulationPatterns2021:
     # "Alter, Tobias B., Lars M. Blank, and Birgitta E. Ebert. “Proteome Regulation Patterns Determine Escherichia Coli Wild-Type and Mutant Phenotypes.” Edited by Joshua E. Elias. mSystems 6, no. 2 (April 27, 2021): 10.1128/msystems.00625-20. https://doi.org/10.1128/msystems.00625-20."
     paperSONDataDir = joinpath(pkgdir(MetXNetHub), "data/PaperSON")
@@ -49,6 +41,57 @@ function __PAMModel_alterProteomeRegulationPatterns2021_builder(build_args...)
     file = sd001Data["data"]["Sheet 1"]["data"]["PaperSON.file"]
     csv_file = joinpath(alterDataDir, file)
     UEData = CSV.read(csv_file, DataFrame)
+
+    ## - --- -- . .- -.-. -.- .-.-. 
+    # MARK: split
+    # We need to split all reactions because 
+    # all variable `x` should have `wx = 0` constraints
+    # Split reactions
+    # S_f v_f + S_b v_b = 0
+    # S_f = S0
+    # S_b = -S0
+    # v = v_f - v_b
+
+    rxns1 = [
+        [string(rxn, "_f") for rxn in rxns0]; 
+        [string(rxn, "_b") for rxn in rxns0]; 
+    ]
+    mets1 = mets0
+    S1 = [
+        S0  -S0
+    ]
+    M, N = size(S1)
+    b1 = b0
+    c1 = zeros(N)
+    lb1 = zeros(N)
+    ub1 = [ub0; abs.(lb0)]
+
+    # toSplit = Int[]
+    # for rxn in UEData[!,"Reaction ID"]
+    #     rxn = replace(rxn, r"_f$" => "")
+    #     rxn = replace(rxn, r"_b$" => "")
+    #     rxni = rxnindex(net0, rxn)
+    #     push!(toSplit, rxni)
+    # end
+    # sort!(toSplit)
+    # tsN = length(toSplit)
+    # N = size(S0, 2)
+    # notToSplit = setdiff(1:N, toSplit)
+
+    # rxns1 = [
+    #     [string(rxns0[rxni], "_f") for rxni in toSplit]; 
+    #     [string(rxns0[rxni], "_b") for rxni in toSplit]; 
+    #     [rxns0[rxni] for rxni in notToSplit]; 
+    # ]
+    # mets1 = mets0
+    # S1 = [
+    #     S0[:, toSplit]  -S0[:, toSplit]  S0[:, notToSplit]
+    # ]
+    # M, N = size(S1)
+    # b1 = b0
+    # c1 = zeros(N)
+    # lb1 = [zeros(tsN); zeros(tsN); lb0[notToSplit]]
+    # ub1 = [ub0[toSplit]; abs.(lb0[toSplit]); ub0[notToSplit]]
 
     ## - --- -- . .- -.-. -.- .-.-. 
     # MARK: f_AE
@@ -89,7 +132,6 @@ function __PAMModel_alterProteomeRegulationPatterns2021_builder(build_args...)
     for (rxni, rxn) in enumerate(rxns1)
         ec_coes[rxni] = get(ec_dict, rxn, 0)
     end
-    ec_coes
 
 
     # f_AE stash variable
@@ -110,14 +152,14 @@ function __PAMModel_alterProteomeRegulationPatterns2021_builder(build_args...)
     # new balance
     # f_UE0 - w_UE * v_s - f_UE  = 0
     w_UE_coes = zeros(size(S2, 2))
-    f_UE0 = 0.17                                # [g / gCDW]
 
-    # ISSUE/QUESTION: can this me extended to multiple nutrients at the same time?
-    Sider = "EX_glc__D_e_b"                     # nutrinet ider
-    Sidx = findfirst(rxns2 .== Sider)
-    uS_max = 8.9                               # [mmol/ h gCDW] max nutrient intake
-    @assert uS_max > 0
-    w_UE_coes[Sidx] = f_UE0 / uS_max            # [h / g mmol] 
+    # ISSUE/QUESTION: 
+    # - can this be extended to multiple nutrients at the same time?
+    # - Maybe having a calibration curve for tot carbon
+    Sidx = findfirst(rxns2 .== u_ider)
+    # nutrient intake
+    @assert u_max > 0
+    w_UE_coes[Sidx] = f_UE0 / u_max            # [h / g mmol] 
     
     # f_AE stash variable
     rxns3 = [rxns2; "f_UE0_stash_rxn"; "f_UE_stash_rxn"]
@@ -135,9 +177,7 @@ function __PAMModel_alterProteomeRegulationPatterns2021_builder(build_args...)
     ## - --- -- . .- -.-. -.- .-.-. 
     # MARK: f_T
     # f_T0 + w_T z - f_T = 0
-    f_T0 = 50.0                     # [mg / gCDW]
     f_T0 = f_T0 * 1e-3              # [g / gCDW]
-    w_T = 36.8                      # [mg h / gCDW]
     w_T = w_T * 1e-3                # [g / gCDW]
 
     zider = "BIOMASS_Ec_iML1515_WT_75p37M_f"    # biomass id
@@ -165,7 +205,6 @@ function __PAMModel_alterProteomeRegulationPatterns2021_builder(build_args...)
 
     # protein allocation constraint
     # f_T + f_UE + f_AE - f_Pc = 0
-    f_Pc = 0.26                                     # [g / gCDW]
     
     w_Pc_coes = zeros(size(S4, 2))
     fidx = findfirst(rxns4 .== "f_AE_stash_rxn")
@@ -179,7 +218,7 @@ function __PAMModel_alterProteomeRegulationPatterns2021_builder(build_args...)
     rxns5 = [rxns4; "f_Pc_stash_rxn"]
     mets5 = [mets4; "Pc_stash_met"]
     lb5 = [lb4; 0]
-    ub5 = [ub4; 1000.0]
+    ub5 = [ub4; f_Pc]
     c5 = [c4; 0]
     b5 = [b4; 0]
     _col0 = zeros(size(S4, 1))
@@ -189,25 +228,38 @@ function __PAMModel_alterProteomeRegulationPatterns2021_builder(build_args...)
     ]
 
     # MARK: MetNet
-    return MetNet(;S=S5, b=b5, lb=lb5, ub=ub5, c=c5, rxns=rxns5, mets=mets5)
+    net0 = MetNet(;S=S5, b=b5, lb=lb5, ub=ub5, c=c5, rxns=rxns5, mets=mets5)
+
+    extras = Dict(
+        "f_UE0" => f_UE0,
+        "u_ider" => u_ider,
+        "u_max" => u_max,
+        "f_T0" => f_T0,
+        "w_T" => w_T,
+        "f_Pc" => f_Pc,
+    )
+
+    return (net0, extras)
 end
 
 function _PAMModel_alterProteomeRegulationPatterns2021_builder(build_args...)
 
     # load
-    net = __PAMModel_alterProteomeRegulationPatterns2021_builder(build_args...)
-    net = _common_format(net)
-    return net
+    net0, extras0 = __PAMModel_alterProteomeRegulationPatterns2021_builder(build_args...)
+    net = _common_format(net0)
     
-    extras!(net, "BIOM", "BIOMASS_Ecoli_core_w_GAM")
-    extras!(net, "EX_GLC", "EX_glc__D_e")
-    extras!(net, "EX_NH4", "EX_nh4_e")
-    extras!(net, "EX_GLU", "EX_glu__L_e")
-    extras!(net, "EX_O2", "EX_o2_e")
-    extras!(net, "EX_CO2", "EX_co2_e")
+    extras!(net, "BIOM", "BIOMASS_Ec_iML1515_WT_75p37M_f")
+    extras!(net, "EX_GLC", "EX_glc__D_e_b")
+    extras!(net, "EX_NH4", "EX_nh4_e_b")
+    extras!(net, "EX_GLU", "EX_glu__L_e_b")
+    extras!(net, "EX_O2", "EX_o2_e_b")
+    extras!(net, "EX_CO2", "EX_co2_e_b")
     extras!(net, "ATPM", "ATPM")
+    for (key, val0) in extras0
+        extras!(net, key, val0)
+    end
 
-    linear_weights!(net, "BIOMASS_Ecoli_core_w_GAM_f", 1.0)
+    linear_weights!(net, "BIOMASS_Ec_iML1515_WT_75p37M_f", 1.0)
 
     return net
 end
